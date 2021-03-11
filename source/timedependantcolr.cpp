db5c160fadb29f640b4affbe05646828d9faaaed
princesinghtomar <singhtomarprince470@gmail.com>
2021-03-12 01:52:11 +0530

    Added some shader effect

--- source/timedependantcolr.cpp
+++ source/timedependantcolr.cpp
@@ -2,7 +2,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include <math.h>
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
@@ -46,21 +46,18 @@ GLFWwindow *initialise()
 const char *vertexShaderSource = "\n"
    "#version 330 core\n"
    "layout (location = 0) in vec3 aPos;\n"
-   "layout (location = 1) in vec3 aColor;\n"
-   "out vec3 ourColor;\n"
    "void main()\n"
    "{\n"
-   "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-   "  ourColor = aColor;\n"
+   "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
    "}\0";
 
 const char *fragmentShaderSource = "\n"
    "#version 330 core\n"
    "out vec4 FragColor;\n"
-   "in vec3 ourColor;\n"
+   "uniform vec4 ourColor;\n"
    "void main()\n"
    "{\n"
-   "  FragColor = vec4(ourColor, 1.0);\n"
+   "   FragColor = ourColor;\n"
    "}\n\0";
 
 static unsigned int CompileShader(unsigned int type,const char *typeShaderSource ){
@@ -115,27 +112,33 @@ int main()
    // ------------------------------------------------------------------
    
    float vertices[] = {
-      // positions         // colors
-      0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-      -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-      0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
-   };  
+      0.5f,  0.5f, 0.0f,  // top right
+      0.5f, -0.5f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f,  // bottom left
+      -0.5f,  0.5f, 0.0f   // top left 
+   };
+   unsigned int indices[] = {  // note that we start from 0!
+      1, 0, 3,   // first triangle
+      3, 2, 1   // second triangle
+   };
 
-   unsigned int VBO, VAO;
+
+
+   unsigned int VBO, VAO, EBO;
    glGenVertexArrays(1, &VAO);
    glGenBuffers(1, &VBO);
+   glGenBuffers(1, &EBO);
    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
    glBindVertexArray(VAO);
 
    glBindBuffer(GL_ARRAY_BUFFER, VBO);
    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6* sizeof(float), (void *)0);
-   glEnableVertexAttribArray(0);
+   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-   // color attribute
-   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
-   glEnableVertexAttribArray(1);
+   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+   glEnableVertexAttribArray(0);
 
    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
    glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -145,7 +148,7 @@ int main()
    glBindVertexArray(0);
 
    // uncomment this call to draw in wireframe polygons.
-   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
    // render loop
    // -----------
@@ -163,8 +166,17 @@ int main()
       // draw our first triangle
       glUseProgram(shaderProgram);
       glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-      glDrawArrays(GL_TRIANGLES, 0, 3);
+      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+      // glDrawArrays(GL_TRIANGLES, 0, 3);
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
       // glBindVertexArray(0); // no need to unbind it every time
+
+      // Change color with time
+      float timeValue = glfwGetTime();
+      float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+      int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+      glUseProgram(shaderProgram);
+      glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
       // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
       // -------------------------------------------------------------------------------
